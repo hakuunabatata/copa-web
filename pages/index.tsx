f650300c44db65417b419bff39ad1f06ef12d927
hakuunabatata <emachadomartins1999@gmail.com>
2022-11-02 15:34:47 -0300

    add fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,21 @@
-
-export default function Home() {
-  return (
-   <h1>Hello World</h1>
-  )
+interface HomeProps {
+  count: number;
 }
+
+export default function Home(props: HomeProps) {
+  const { count = 0 } = { count: 2 };
+
+  return <h1>Count: {props.count}</h1>;
+}
+
+export const getServerSideProps = async () => {
+  const { count } = await fetch("http://localhost:3333/pools/count").then(
+    (res) => {
+      return res.json();
+    }
+  );
+
+  return {
+    props: { count }
+  };
+};
